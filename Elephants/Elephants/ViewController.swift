fa37cdd15b16904558d61522f3dd085604b26fb1
Wilson Baraban Filho <fbaraban@gmail.com>
2021-10-16 22:03:06 -0700

    Get response and display to screen

--- Elephants/Elephants/ViewController.swift
+++ Elephants/Elephants/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let api = API()
+    var elephants : [Elephant] = []
+    var error = ""
 
     lazy var uitv_TableView : UITableView = {
        
-        let tableView = UITableView()
+        let tableView = UITableView(frame: self.view.bounds)
         tableView.dataSource = self
         tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         return tableView
     }()
     
@@ -21,16 +26,50 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        self.view.addSubview(self.uitv_TableView)
+        
+        api.getElephants(urlString: api.setElephantsURL(), method: .GET) { elephants in
+            self.elephants = elephants
+            DispatchQueue.main.async {
+                self.uitv_TableView.reloadData()
+            }
+            print("ELEPHANTS: \(self.elephants.count)")
+        } errorReturned: { error in
+            self.error = error
+        }
+
     }
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return self.elephants.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        let elephant = self.elephants[indexPath.row]
+        if let imageURL = elephant.image {
+            if let url = URL(string: imageURL) {
+                if let data = try? Data(contentsOf: url) {
+                    content.image = UIImage(data: data)
+                    content.imageProperties.reservedLayoutSize = CGSize(width: 50.0, height: 50.0)
+                    content.imageProperties.maximumSize = CGSize(width: 50.0, height: 50.0)
+                    //content.imageProperties.cornerRadius = (content.image?.size.height)! / 2
+                }
+            }
+        }
+        if let texto = elephant.name {
+            content.text = texto
+        }
+        if let note = elephant.note {
+            content.secondaryText = note
+        }
+        cell.contentConfiguration = content
+        
+        return cell
     }
     
     
